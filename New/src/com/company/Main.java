fe090587983bcdba596672c14cd84d5f64e89723
Tarannu <tarannum.aiub@gmail.com>
2020-09-21 23:15:01 -0400

    doubly linked list with error

--- New/src/com/company/Main.java
+++ New/src/com/company/Main.java
@@ -6,33 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-    isAnagram("ball","call");
+    
 
-    }
-    static boolean isAnagram(String a, String b) {
-        // Complete the function
-        if(a.length()!=b.length()) return false;
-        a=a.toLowerCase();
-        b=b.toLowerCase();
-        int count=0;
-
-        for(char c='a';c<='z';c++){
-            for(int i=0;i<a.length();i++){
-                if(a.charAt(i)==c){
-                    count++;
-                }
-                if(b.charAt(i)==c){
-                    count--;
-                }
-            }
-
-
-            if(count!=0) {
-                return false;
-            }
-        }
-
-        return true;
     }
 
 }
