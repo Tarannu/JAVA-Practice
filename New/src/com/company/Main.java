77dfe92667ca6edaeed0c7aefe433f29b509436a
Tarannu <tarannum.aiub@gmail.com>
2020-09-21 23:03:42 -0400

    string comparision

--- New/src/com/company/Main.java
+++ New/src/com/company/Main.java
@@ -4,25 +4,37 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static string main(String s , int k) {
 	// write your code here
 
-                Scanner sc=new Scanner(System.in);
-                String A=sc.next();
-                /* Enter your code here. Print output to STDOUT. */
-        String B = new StringBuffer(A).reverse().toString();
+            s="welcometojava"; k=3;
+            String smallest = "";
+            String largest = "";
 
-        if (B.equals(A))
-        {
-            System.out.println("Yes");
-        }
-        else
-        {
-            System.out.println("No");
-        }
+            // Complete the function
+            // 'smallest' must be the lexicographically smallest substring of length 'k'
+            // 'largest' must be the lexicographically largest substring of length 'k'
+            for(int i=0;i<=s.length()-k;i++){
+                String sub=s.substring(i,i+k);
+
+                if(sub.compareTo(smallest)<0||smallest.isEmpty()){
+                    smallest=sub;
+
+                }
+                if(sub.compareTo(largest)>0||smallest.isEmpty()){
+                    largest=sub;
+
+                }
+
 
             }
+
+
+            System.out.println(smallest + "\n" + largest);
         }
+
+}
+
 
 
 
