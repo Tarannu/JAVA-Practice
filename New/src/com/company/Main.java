e85dfb3595aa07e3c31b7c26b664a4b50fa687d5
Tarannu <tarannum.aiub@gmail.com>
2020-09-21 22:59:27 -0400

    String Reversal using string buffer

--- New/src/com/company/Main.java
+++ New/src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    }
-}
+
+                Scanner sc=new Scanner(System.in);
+                String A=sc.next();
+                /* Enter your code here. Print output to STDOUT. */
+        String B = new StringBuffer(A).reverse().toString();
+
+        if (B.equals(A))
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+
+            }
+        }
+
+
+
+
+
